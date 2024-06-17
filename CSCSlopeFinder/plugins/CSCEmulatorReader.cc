b335c7f1fd697aed954b5b592d16cfbd2cd49fa7
Devin <aebid@tamu.edu>
2024-06-17 09:40:51 +0200

    Started an emulator reader, very similar to the LCT reader from unpacker

---
+++ CSCSlopeFinder/plugins/CSCEmulatorReader.cc
@@ -0,0 +1,191 @@
+#include <assert.h>
+#include <memory>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+//user include files below
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/Utils/interface/StringCutObjectSelector.h"
+
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
+
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+
+#include "DataFormats/MuonReco/interface/MuonSelectors.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2D.h"
+#include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h"
+#include "DataFormats/GEMRecHit/interface/GEMRecHitCollection.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
+
+#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
+#include "Geometry/GEMGeometry/interface/GEMGeometry.h"
+#include "Geometry/GEMGeometry/interface/GEMEtaPartitionSpecs.h"
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+#include "Geometry/CommonTopologies/interface/StripTopology.h"
+
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+
+#include "TTree.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TString.h"
+#include "TGraphAsymmErrors.h"
+#include "TLorentzVector.h"
+
+using namespace std;
+using namespace edm;
+
+
+struct EmuData
+{
+  void init();
+  TTree* book(TTree *t);
+  //============ Muon Info ================//
+  int muon_charge; float muon_pt; float muon_eta;
+  unsigned long long evtNum; unsigned long long lumiBlock; int runNum;
+
+};
+
+void EmuData::init()
+{
+  //=========== Muon Info ===============//
+  float value = 99999;
+  muon_charge = value; muon_pt = value; muon_eta = value;
+  evtNum = value; lumiBlock = value; runNum = value;
+
+}
+
+TTree* EmuData::book(TTree *t){
+  edm::Service< TFileService > fs;
+  t = fs->make<TTree>("EmuData", "EmuData");
+
+  //=========== Muon Info =============//
+  t->Branch("muon_charge", &muon_charge); t->Branch("muon_pt", &muon_pt); t->Branch("muon_eta", &muon_eta);
+  t->Branch("evtNum", &evtNum); t->Branch("lumiBlock", &lumiBlock); t->Branch("runNum", &runNum);
+
+  return t;
+}
+
+class CSCEmulatorReader : public edm::one::EDAnalyzer<> {
+public:
+  explicit CSCEmulatorReader(const edm::ParameterSet&);
+  ~CSCEmulatorReader(){};
+
+private:
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void beginJob() ;
+  virtual void endJob() ;
+
+  edm::EDGetTokenT<CSCCorrelatedLCTDigiCollection> co_token;
+
+  edm::EDGetTokenT<edm::View<reco::Muon> > muons_;
+
+  edm::Service<TFileService> fs;
+  MuonServiceProxy* theService_;
+    
+  edm::ESHandle<CSCGeometry> CSCGeometry_;
+
+  bool debug;
+
+  EmuData data_;
+  TTree* tree;
+
+  bool isMC;
+
+  const edm::ESGetToken<CSCGeometry, MuonGeometryRecord> cscGeomToken_;
+};
+
+
+CSCEmulatorReader::CSCEmulatorReader(const edm::ParameterSet& iConfig)
+  : cscGeomToken_(esConsumes())
+{
+  cout << "Begin CSCEmulatorReader" << endl;
+  edm::ParameterSet serviceParameters = iConfig.getParameter<edm::ParameterSet>("ServiceParameters");
+  theService_ = new MuonServiceProxy(serviceParameters, consumesCollector());
+  muons_ = consumes<View<reco::Muon> >(iConfig.getParameter<InputTag>("muons"));
+  co_token = consumes<CSCCorrelatedLCTDigiCollection>(iConfig.getParameter<edm::InputTag>("emu_corrlctDigiTag"));
+
+
+  debug = iConfig.getParameter<bool>("debug");
+  std::cout << "debug " << debug << std::endl;
+
+  tree = data_.book(tree);
+
+}
+
+
+void
+CSCEmulatorReader::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+
+  CSCGeometry_ = &iSetup.getData(cscGeomToken_);
+
+  theService_->update(iSetup);
+  auto propagator = theService_->propagator("SteppingHelixPropagatorAny");
+
+  isMC = false;
+  if (! iEvent.eventAuxiliary().isRealData()) isMC = true;
+  edm::Handle<View<reco::Muon> > muons;
+  edm::Handle<CSCCorrelatedLCTDigiCollection> correlatedlcts;
+  iEvent.getByToken(co_token, correlatedlcts);
+
+  if (debug) cout << "New! EventNumber = " << iEvent.eventAuxiliary().event() << " LumiBlock = " << iEvent.eventAuxiliary().luminosityBlock() << " RunNumber = " << iEvent.run() << endl;
+  if (! iEvent.getByToken(muons_, muons)) return;
+  if (debug) cout << "There are " << muons->size() << " muons in event" << endl;
+  if (muons->size() == 0) return;
+
+  //cout << "Starting the corr LCT search" << endl;
+  cout << "New Event" << endl;
+  for (CSCCorrelatedLCTDigiCollection::DigiRangeIterator j = correlatedlcts->begin(); j != correlatedlcts->end(); j++){
+    //cout << "Looping corr lcts" << endl;
+    cout << "New Chamber " << (*j).first << endl;
+    if ((*j).first.station() != 1 or (*j).first.ring() != 1) continue;
+    std::vector<CSCCorrelatedLCTDigi>::const_iterator digiItr = (*j).second.first;
+    std::vector<CSCCorrelatedLCTDigi>::const_iterator last = (*j).second.second;
+    for (; digiItr != last; ++digiItr) {
+      cout << "Found a LCT" << endl;
+      cout << "Strip:        " << digiItr->getStrip() << endl;
+      cout << "Slope:        " << digiItr->getSlope() << endl;
+      cout << "Bend          " << digiItr->getBend() << endl;
+      cout << "Run2 Pattern: " << digiItr->getPattern() << endl;
+      cout << "Run3 Pattern: " << digiItr->getRun3Pattern() << endl;
+    }
+  }
+}
+
+
+
+
+void CSCEmulatorReader::beginJob(){
+  cout << "Begin job!" << endl;
+  cout << "Ended Begin Job, starting Event Loop" << endl;
+}
+void CSCEmulatorReader::endJob(){}
+
+DEFINE_FWK_MODULE(CSCEmulatorReader);
