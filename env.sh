e3e7d59283fb26cfb1feb35703f379e2d55dde77
Valeria D'Amante <valeriadamante@gmail.com>
2025-10-27 16:59:08 +0100

    fixed env

--- env.sh
+++ env.sh
@@ -1,3 +1,16 @@
+apply_cmssw_customization_steps() {
+    run_cmd git cms-addpkg DataFormats/LCTDebug
+    run_cmd git cms-addpkg DataFormats/CSCDigi
+    run_cmd git cms-addpkg DataFormats/L1TMuon
+    run_cmd git cms-addpkg Configuration/Generator
+    run_cmd git cms-addpkg Configuration/StandardSequences
+    run_cmd git cms-addpkg CondFormats/CSCObjects
+    run_cmd git cms-addpkg CalibMuon/CSCCalibration
+    run_cmd git cms-addpkg EventFilter/CSCRawToDigi
+    run_cmd mkdir GEMCSCTriggerTest
+    run_cmd ln -s "$ANALYSIS_PATH/CSCSlopeFinder" GEMCSCTriggerTest/CSCSlopeFinder
+}
+
 
 CMSSW_VERSION="CMSSW_14_2_0_pre1"
 
@@ -9,7 +22,9 @@ action() {
     echo "Running action with CMSSW version: $CMSSW_VERSION"
 
     source $ANALYSIS_PATH/GEM-CSC-trg-dev/env.sh "$this_file_path" "$CMSSW_VERSION" "$@"
+    ln -fs "$ANALYSIS_PATH/GEM-CSC-trg-dev/luts" "$ANALYSIS_PATH/CSCSlopeFinder/luts"
 }
 
 action "$@"
 unset -f action
+unset -f apply_cmssw_customization_steps
